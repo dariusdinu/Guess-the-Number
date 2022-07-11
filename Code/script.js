75c96b8fb28d7c8a4e89946f2ba8654e30aeb385
dariusdinu <75120006+dariusdinu@users.noreply.github.com>
2022-07-11 21:17:26 +0300

    Update script.js

--- Code/script.js
+++ Code/script.js
@@ -1,16 +1,5 @@
 'use strict';
 
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number! 🎉';
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
-
 const displayMessage = function (message) {
   document.querySelector('.message').textContent = message;
 };
